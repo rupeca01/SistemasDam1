4d01446105a4e16d71242e715ce549ebcc3f6526
DESKTOP-VVCB3F0\Ruben <rubenpecasti@gmail.com>
2020-12-26 00:47:35 +0100

    modifico 5.sh

--- Tema6Tarea3/5.sh
+++ Tema6Tarea3/5.sh
@@ -8,22 +8,25 @@ lin=0
 prowin=0
 prolin=0
 
-while [ $numlinea -le $maxlineas ];
+while [ $numlinea -le $maxlineas ]
 do
 	linea=`cat listado.txt | head -$numlinea | tail -1`
 	user=`echo $linea | awk '{print $1}'`
 	os=`echo $linea | awk '{print $2}'`
 	pro=`echo $linea | awk '{print $3]'`
 
-	if [ $os = "Windows" ];
+	if [ $os = "Windows" ]
 	then
 		win=$((win+1))
 		prowin=$((prowin+pro))
 	fi
 
-	if [ $os = "Linux" ];
+	if [ $os = "Linux" ]
 	then
 		lin=$((lin+1))
 		prolin=$((prolin+pro))
 	fi
+	numlinea=$((numlinea+1))
 done
+echo "Windows: $win $prowin"
+echo "Linux: $lin $prolin"
