7515d7eed3a4e23192b6a66c9b8dc6fff15bf07e
DESKTOP-VVCB3F0\Ruben <rubenpecasti@gmail.com>
2020-12-25 23:58:50 +0100

    Añado 1.sh

---
+++ Tema6Tarea3/1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+maxlineas=`cat nombres.txt | wc -l`
+linea=1
+
+while [ $linea -le $maxlineas ]
+do
+	nombre=`cat nombres.txt | head -$linea | tail -1`
+	mkdir "$nombre"
+	cd "$nombre"
+	for i in `seq 1  $1`
+	do	
+		mkdir personal$i
+	done
+	cd ..
+	linea=$((linea+1))
+done
